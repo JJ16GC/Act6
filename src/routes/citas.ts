d79b8043126de75cd6cf1cbbb24d9627d6c8c929
JJ <jj16gc@gmail.com>
2023-07-14 21:09:23 -0500

    Segundo commit

--- src/routes/citas.ts
+++ src/routes/citas.ts
@@ -5,7 +5,8 @@ const router = Router();
 //model
 
 import Citas from "../models/Citas";
-import Personas from "../models/Personas";
+import Persona from "../models/Personas";
+import Doctores from "../models/Doctores";
 
 router
   .route("/create")
@@ -13,34 +14,28 @@ router
     res.render("citas/create");
   })
   .post(async (req: Request, res: Response) => {
+    const personas = await Persona.find({}).lean();
+    const doctor = await Doctores.find({}).lean();
     const { cedula, especialidad } = req.body;
-
-    const newCita = new Citas({
-      cedula,
-      especialidad,
-    });
-    console.log();
-    await newCita.save();
-    res.redirect("/citas/list");
+    const resultadop = await Persona.findOne({ cedula });
+    const resultadod = await Doctores.findOne({ especialidad });
+    if (resultadop) {
+      if (resultadod) {
+        console.log(personas);
+        const newCita = new Citas({
+          cedula,
+          especialidad,
+        });
+        console.log();
+        await newCita.save();
+        res.redirect("/citas/list");
+      } else {
+        res.render("citas/exceptions/error_doc");
+      }
+    } else {
+      res.render("citas/exceptions/error_per");
+    }
   });
-
-  router
-  .route("/detalle/:id")
-  .get(async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const citas = await Citas.findById(id).lean();
-    res.render("citas/detalle", { citas });
-  })
-  .post(async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { cedula, especialidad } = req.body;
-    await Citas.findByIdAndUpdate(id, {
-      cedula,
-      especialidad,
-    }).lean();
-    res.redirect("/citas/list");
-  });
-
 
 router.route("/list").get(async (req: Request, res: Response) => {
   const citas = await Citas.find({}).lean();
@@ -70,4 +65,24 @@ router
     }).lean();
     res.redirect("/citas/list");
   });
+
+router
+  .route("/detalle/:cedula/:especialidad")
+  .get(async (req: Request, res: Response) => {
+    const { cedula, especialidad } = req.params;
+    const citas = await Citas.findOne({ cedula }).lean();
+    const personas = await Persona.findOne({ cedula }).lean();
+    const doctor = await Doctores.findOne({ especialidad }).lean();
+    res.render("citas/detalle", { citas, personas, doctor });
+  })
+  .post(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { cedula, especialidad } = req.body;
+    await Citas.findByIdAndUpdate(id, {
+      cedula,
+      especialidad,
+    }).lean();
+    res.redirect("/citas/list");
+  });
+
 export default router;
